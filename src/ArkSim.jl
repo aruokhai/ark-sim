ac37d2f8f5e4736a5a6853318f9ea6f541917e53
joshua <joshuaaruokhaitech@gmail.com>
2024-12-31 12:32:13 +0100

    added a more realistic Txrate, to match length of a round which is a minute

--- src/ArkSim.jl
+++ src/ArkSim.jl
@@ -12,7 +12,7 @@ module ArkSim
     export main
 
     function main()
-        # Txrate: 0.59/month, 10% high, 60% medium, 30% low, avg balance 1000 usd, 5000 user, initial amount of 1btc, round is 1 minute, timout is 30 days = 43,200 minutes
+        # Txrate: 0.000013657/minute = 0.59/month, 10% high, 60% medium, 30% low, avg balance 1000 usd, 5000 user, initial amount of 1btc, round is 1 minute, timout is 30 days = 43,200 minutes
         # run simulation for 1 year
         visa_network = NetworkConfig(5000, 100_000_000_000, 43_200, 1_000_000, 43_200, 0.59, Dict("high" => 0.1, "medium" => 0.6, "low" => 0.3))
         adata, mdata = run_network(visa_network, 518400)
